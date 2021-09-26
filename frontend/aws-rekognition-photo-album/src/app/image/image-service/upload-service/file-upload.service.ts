4be73469496dd165417d564a75b379468ce69c37
yovo <yovo.manolov@abv.bg>
2021-09-26 23:46:10 +0300

    .

--- frontend/aws-rekognition-photo-album/src/app/image/image-service/upload-service/file-upload.service.ts
+++ frontend/aws-rekognition-photo-album/src/app/image/image-service/upload-service/file-upload.service.ts
@@ -1,20 +1,28 @@
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const uploadUrl = 'http://localhost:8000/awsRekognitionPhotoAlbum/uploadImage';
+
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
+  public uploadImage(imageUrl: string): Observable<string | any> {
+    const formData: FormData = new FormData();
+    formData.append("filePath", imageUrl)
 
-  public uploadImage(image: File): Observable<string | any> {
-    const formData = new FormData();
-
-    formData.append('image', image);
-
-    return this.http.post('/api/v1/image-upload', formData).pipe(map(((json: any) => json.imageUrl)));
+    return this.httpClient.post(uploadUrl, formData).pipe(
+      catchError(
+        (err) => {
+          console.error(err);
+          throw err;
+        }
+      )
+    );
   }
 
 }
