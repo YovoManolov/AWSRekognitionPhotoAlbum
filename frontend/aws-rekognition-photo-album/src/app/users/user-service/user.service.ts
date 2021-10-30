6e3988ec547779111980b00b839f7b6afeb918cc
yovo <yovo.manolov@abv.bg>
2021-10-31 01:03:41 +0300

    .

--- frontend/aws-rekognition-photo-album/src/app/users/user-service/user.service.ts
+++ frontend/aws-rekognition-photo-album/src/app/users/user-service/user.service.ts
@@ -1,7 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ReturnStatement } from '@angular/compiler';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 const baseUrl = 'http://localhost:8000/awsRekognitionPhotoAlbum/users';
 
@@ -10,25 +7,6 @@ const baseUrl = 'http://localhost:8000/awsRekognitionPhotoAlbum/users';
 })
 export class UserService {
 
-  visibleImages: any[] = [];
-  constructor(private http: HttpClient) { }
-
-  headers = new HttpHeaders({
-    'Content-Type': 'application/json'
-  });
-
-
-  signUp(data: any): Observable<any> {
-    return this.http.post(
-      baseUrl,
-      JSON.stringify(data),
-      { headers: this.headers }
-    )
-  }
-
-  // signIn(): Observable<any> {
-  //   return;
-  // }
 
 }
 
