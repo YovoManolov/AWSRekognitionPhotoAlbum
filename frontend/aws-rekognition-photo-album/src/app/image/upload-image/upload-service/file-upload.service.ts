9ede2e54c440fe4a9308c88c87f901658cc88b52
yovo <yovo.manolov@abv.bg>
2021-11-27 19:35:02 +0200

    remove unnecessery method from FileUploadService.

--- frontend/aws-rekognition-photo-album/src/app/image/upload-image/upload-service/file-upload.service.ts
+++ frontend/aws-rekognition-photo-album/src/app/image/upload-image/upload-service/file-upload.service.ts
@@ -11,10 +11,6 @@ export class FileUploadService {
 
   constructor(private http: HttpClient) { }
 
-  public uploadImage(value: any): Observable<string | any> {
-    return this.http.post(uploadUrl, value);
-  }
-
   public upload(fullFilePath: any, base64ImageSrc: any, userEmail: any) {
     let formData: any = new FormData();
     formData.append("base64Image", base64ImageSrc);
